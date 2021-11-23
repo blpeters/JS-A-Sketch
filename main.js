126a01145cc3c91bf1d07b282a6b7c6ca4fc5ca7
blpeters <brett.peters@utexas.edu>
2021-11-22 23:05:14 -0600

    added reset grid functionality with user input

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
 // TODO
 //Create a 16x16 grid of html divs using JavaScript
 
-    //Figure out how to move the clear button/other controls to be below the canvas
-    //After clearing add a popup that requests the user input for grid size and updates grid accordingly. max 100
-    //Make sure total grid size stays the same.
+    //Figure out how to move the clear button/other controls to be below the canvas.
     //Add rainbow option
     //add grayscale shading option.
     //create CSS styling and illustrations for etch-a-sketch look. Goal is to look like an etch-a-sketch sitting on a table.
     
     let gridSize = 50;
-    drawGridCanvas(gridSize);
-
+    setupDefaultGrid(gridSize);
+ 
     let clearBtn = document.querySelector("#clear-grid");
-    clearBtn.addEventListener('click', clearGrid);
+    clearBtn.addEventListener('click', () => {
+        clearGrid(); 
+        resetGrid();
+    });
     
-    function drawGridCanvas(size) {
+    function resetGrid() {
+        let userGridSizeInput = 0;
+        do {
+            userGridSizeInput = prompt("New Grid Size?: ");
+        } while(Number.isInteger(userGridSizeInput) === false && (userGridSizeInput < 1 || userGridSizeInput > 100)); 
+              
+        //Deletes existing square divs from the DOM to make room for new grid in the container div.
+        let squares = document.querySelectorAll(".square");
+        squares.forEach((square) => square.remove());
+        divideGrid(userGridSizeInput);      
+    }
+
+    function setupDefaultGrid(size) {
         let grid = document.createElement("div");
         grid.classList.add('grid');
         document.body.appendChild(grid);
+        divideGrid(size);
+    }
 
+    function divideGrid(size) {
+        let grid = document.querySelector(".grid");
         grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         grid.style.gridTemplatecolumns = `repeat(${size}, 1fr)`;
         grid.style.gridAutoFlow = "column";
-
-
-        drawGridSquares(grid, gridSize);
+        
+        drawGridSquares(grid, size);
     }
     
     function drawGridSquares(grid, numberOfSquares) {
-        console.log(numberOfSquares);
         for (let j = 0; j < numberOfSquares; j++) {
             for (let i = 0; i < numberOfSquares; i++) {
                 let gridSquare = document.createElement("div");
@@ -47,5 +62,5 @@
 
     function clearGrid() {
         let clearSquares = document.querySelectorAll(".square");
-        clearSquares.forEach((square) => square.style.backgroundColor = "rgb(196, 194, 194)");
+        clearSquares.forEach((square) => square.style.removeProperty("backgroundColor"));
     }
